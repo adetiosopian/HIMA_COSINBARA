55725199acf421cdc0b6ed996a4f89e7223e354b
Ade Tio Sopian <adetiosopian@gmail.com>
2025-11-25 14:46:10 +0700

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import './assets/main.css'
-import { createPinia } from 'pinia'
-
-createApp(App).use(router).use(createPinia()).mount('#app')
